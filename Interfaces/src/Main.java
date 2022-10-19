65216207d4169417283ac31cd59363c0196d5191
Berkay Alişan <Berkay Alian@10.242.6.152>
2022-10-19 15:33:42 +0300

    week3-1

---
+++ Interfaces/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//ICustomerDal customerDal =new OracleCustomerDal();
+		CustomerManager customerManager =new CustomerManager(new MySqlCustomerDal());
+		customerManager.add();
+		//customerManager.customerDal =new OracleCustomerDal();
+
+	}
+
+}
